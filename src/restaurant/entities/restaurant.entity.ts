974e95c007b795e015b9b3e7c00ceb345c86bd7a
Aio <AioDev.JS@gmail.com>
2021-05-05 11:59:00 +0900

    #3.6 Optional Types and defaultValue, nullable

--- src/restaurant/entities/restaurant.entity.ts
+++ src/restaurant/entities/restaurant.entity.ts
@@ -1,5 +1,5 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsString, Length } from 'class-validator';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @InputType({ isAbstract: true }) // input type이 스키마에 포함되지 않는다. 추상 확장 , OmitType에 명시하거나 여기에 적던가 택1
 @ObjectType()
@@ -15,8 +15,9 @@ export class Restaurant {
   @Length(5)
   name: string;
 
-  @Field((type) => Boolean)
-  @Column()
+  @Field((type) => Boolean, { nullable: true })
+  @Column({ default: true })
+  @IsOptional()
   @IsBoolean()
   isVegan: boolean;
 
@@ -25,13 +26,15 @@ export class Restaurant {
   @IsString()
   address: string;
 
-  @Field((type) => String)
-  @Column()
-  @IsString()
-  ownersName: string;
+  // @Field((type) => String)
+  // @Column()
+  // @IsString()
+  // @IsOptional()
+  // ownersName: string;
 
-  @Field((type) => String)
-  @Column()
-  @IsString()
-  categoryName: string;
+  // @Field((type) => String)
+  // @Column()
+  // @IsString()
+  // @IsOptional()
+  // categoryName: string;
 }
