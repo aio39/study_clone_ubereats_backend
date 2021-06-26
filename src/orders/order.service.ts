58a5d964bd87d18ebd43df413ae151037c2a69ad
Aio <AioDev.JS@gmail.com>
2021-06-26 20:01:59 +0900

    GH-3 CreateOrder part Six

--- src/orders/order.service.ts
+++ src/orders/order.service.ts
@@ -24,57 +24,74 @@ export class OrderService {
     customer: User,
     { restaurantId, items }: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
-    const restaurant = await this.restaurants.findOne(restaurantId);
-    if (!restaurant) {
-      return {
-        ok: false,
-        error: 'Restaurant not found',
-      };
-    }
-    //  foreach 말고 for loof 사용
-    for (const item of items) {
-      const dish = await this.dishes.findOne(item.dishId);
-      if (!dish) {
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
         return {
           ok: false,
-          error: 'dish not found',
+          error: 'Restaurant not found',
         };
       }
-      console.log(`Dish Price: ${dish.price}`);
-      for (const itemOption of item.options) {
-        console.log(itemOption);
-        const dishOption = dish.options.find((dishOption) => {
-          console.log(dishOption);
-          return dishOption.name === itemOption.name;
-        });
-        if (dishOption) {
-          if (dishOption.extra) {
-            console.log(dishOption.extra);
-          } else {
-            const dishOptionChoice = dishOption.choices.find(
-              (optionChoice) => optionChoice.name === itemOption.choice,
-            );
-            if (dishOptionChoice) {
-              if (dishOptionChoice.extra) {
-                console.log(dishOptionChoice.extra);
+      let orderFinalPrice = 0;
+      const orderItems: OrderItem[] = [];
+      //  foreach 말고 for loof 사용
+      for (const item of items) {
+        const dish = await this.dishes.findOne(item.dishId);
+        if (!dish) {
+          return {
+            ok: false,
+            error: 'dish not found',
+          };
+        }
+
+        let dishFinalPrice = dish.price;
+        for (const itemOption of item.options) {
+          console.log(itemOption);
+          const dishOption = dish.options.find((dishOption) => {
+            console.log(dishOption);
+            return dishOption.name === itemOption.name;
+          });
+          if (dishOption) {
+            if (dishOption.extra) {
+              dishFinalPrice += dishOption.extra;
+            } else {
+              const dishOptionChoice = dishOption.choices.find(
+                (optionChoice) => optionChoice.name === itemOption.choice,
+              );
+              if (dishOptionChoice) {
+                if (dishOptionChoice.extra) {
+                  dishFinalPrice += dishOptionChoice.extra;
+                }
               }
             }
           }
         }
+        orderFinalPrice += dishFinalPrice;
+        const orderItem = await this.orderItems.save(
+          this.orderItems.create({
+            dish,
+            options: item.options,
+          }),
+        );
+        orderItems.push(orderItem);
       }
-      // await this.orderItems.save(
-      //   this.orderItems.create({
-      //     dish,
-      //     options: item.options,
-      //   }),
-      // );
+      console.log(orderFinalPrice);
+      await this.orders.save(
+        this.orders.create({
+          customer,
+          restaurant,
+          total: orderFinalPrice,
+          items: orderItems,
+        }),
+      );
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not create Order',
+      };
     }
-    // const order = await this.orders.save(
-    //   this.orders.create({
-    //     customer,
-    //     restaurant,
-    //   }),
-    // );
-    // console.log(order);
   }
 }
